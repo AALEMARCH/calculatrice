e1ceee7767628b9835cf4da14eb18ce191f1a502
Antoine Lemarchand <lemarchand.antoine00@gmail.com>
2022-11-20 20:20:46 +0100

    implémentation du dark mode en fonction de l'heure de la journée / gestion de bug concernant des dépassement de conteneur / optimisation du code

--- js/historique.js
+++ js/historique.js
@@ -1,4 +1,6 @@
 // fonctionnalité: historique de calcul
+
+// ciblage du boutons de changement de page et redirection
 const btnlocationCalculatrice =
   document.getElementsByClassName("btnCalculatrice");
 
@@ -7,6 +9,34 @@ btnlocationCalculatrice[0].addEventListener("click", (e) => {
   window.location.href = "index.html";
 });
 
+// fonctionnalité dark/light mode
+const darkAndLight = () => {
+  const date = new Date();
+  const hour = date.getHours();
+
+  if (hour > 6 && hour < 20) {
+    document.documentElement.style.setProperty("--color-1", "#D1D9E2");
+    document.documentElement.style.setProperty("--color-2", "#6F7476");
+    document.documentElement.style.setProperty("--color-3", "#D6D6D6");
+    document.documentElement.style.setProperty("--color-4", "#313B47");
+    document.documentElement.style.setProperty("--color-5", "#F1F1F3");
+    document.documentElement.style.setProperty("--color-6", "#F5F5F5");
+    document.documentElement.style.setProperty("--color-7", "#000000");
+    document.documentElement.style.setProperty("--border", "none");
+  } else {
+    document.documentElement.style.setProperty("--color-1", "white");
+    document.documentElement.style.setProperty("--color-2", "white");
+    document.documentElement.style.setProperty("--color-3", "#313B47");
+    document.documentElement.style.setProperty("--color-4", "#000000");
+    document.documentElement.style.setProperty("--color-5", "#114274");
+    document.documentElement.style.setProperty("--color-6", "#313B47");
+    document.documentElement.style.setProperty("--color-7", "#000000");
+    document.documentElement.style.setProperty("--border", "1px solid #000000");
+  }
+};
+darkAndLight();
+
+// suppression de l'historique de calcul
 const btnBottom = document.getElementsByClassName("btnBottom");
 
 btnBottom[0].addEventListener("click", (e) => {
@@ -15,6 +45,7 @@ btnBottom[0].addEventListener("click", (e) => {
   lsHistori.innerHTML = "";
 });
 
+// Récupération des donnée du local storage et création d'une div pour chaque résultats
 let calculRecovery = JSON.parse(localStorage.getItem("operation"));
 
 let lsHistori = document.querySelector(".lsHistori");
@@ -24,6 +55,7 @@ if (calculRecovery != null) {
     const lsHistori = document.getElementsByClassName("lsHistori");
 
     let calculItem = document.createElement("div");
+    calculItem.setAttribute("class", "lsHistori__divCalcul");
 
     calculItem.innerHTML = `Calcul n°${n++} : ${element.operation} = ${
       element.resultat
--- js/index.js
+++ js/index.js
@@ -27,6 +27,43 @@ document.addEventListener("click", (e) => {
   calculatrice(valeur);
 });
 
+// fonctionnalité dark/light mode
+const darkAndLight = () => {
+  const date = new Date();
+  const hour = date.getHours();
+
+  if (hour > 6 && hour < 20) {
+    document.documentElement.style.setProperty("--color-1", "#D1D9E2");
+    document.documentElement.style.setProperty("--color-2", "#6F7476");
+    document.documentElement.style.setProperty("--color-3", "#D6D6D6");
+    document.documentElement.style.setProperty("--color-4", "#313B47");
+    document.documentElement.style.setProperty("--color-5", "#F1F1F3");
+    document.documentElement.style.setProperty("--color-6", "#F5F5F5");
+    document.documentElement.style.setProperty("--color-7", "#000000");
+    document.documentElement.style.setProperty("--border", "none");
+  } else {
+    document.documentElement.style.setProperty("--color-1", "white");
+    document.documentElement.style.setProperty("--color-2", "white");
+    document.documentElement.style.setProperty("--color-3", "#313B47");
+    document.documentElement.style.setProperty("--color-4", "#000000");
+    document.documentElement.style.setProperty("--color-5", "#114274");
+    document.documentElement.style.setProperty("--color-6", "#313B47");
+    document.documentElement.style.setProperty("--color-7", "#000000");
+    document.documentElement.style.setProperty("--border", "1px solid #000000");
+  }
+};
+darkAndLight();
+
+// const btnDark = document.getElementsByClassName("btnDark");
+// btnDark[0].addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (e === true) {
+//     console.log("test");
+//   } else {
+//     console.log("fin test");
+//   }
+// });
+
 // fonction de vérification du dernier caractère de l'operation
 const verifLastCaract = (valeur) => {
   const str = screen.textContent;
@@ -62,6 +99,40 @@ const gestionParenthese = () => {
   for (let i = findCloseP.length; i < findOpenP.length; i++) {
     if (findCloseP.length < findOpenP.length) {
       screen.textContent += ")";
+    }
+  }
+};
+
+// fonction de réduction de code pour les different boutton egal, evaluation des operation et envoie sur le local storage
+const equal = (lastC) => {
+  if (
+    lastC != "/" &&
+    lastC != "*" &&
+    lastC != "-" &&
+    lastC != "+" &&
+    lastC != "." &&
+    lastC != ""
+  ) {
+    gestionParenthese();
+
+    const calcul = eval(screen.textContent);
+    result.textContent = calcul.toLocaleString();
+    screen.textContent = `(${screen.textContent})`;
+
+    let tabl = {
+      operation: screen.textContent,
+      resultat: calcul,
+    };
+
+    let operationLocalStorage = JSON.parse(localStorage.getItem("operation"));
+
+    if (operationLocalStorage) {
+      operationLocalStorage.push(tabl);
+      localStorage.setItem("operation", JSON.stringify(operationLocalStorage));
+    } else {
+      operationLocalStorage = [];
+      operationLocalStorage.push(tabl);
+      localStorage.setItem("operation", JSON.stringify(operationLocalStorage));
     }
   }
 };
@@ -133,44 +204,7 @@ const calculatrice = (valeur) => {
         verifLastCaract(valeur);
         break;
       case "187":
-        if (
-          lastC != "/" &&
-          lastC != "*" &&
-          lastC != "-" &&
-          lastC != "+" &&
-          lastC != "." &&
-          lastC != ""
-        ) {
-          gestionParenthese();
-
-          const calcul = eval(screen.textContent);
-          result.textContent = calcul.toLocaleString();
-          screen.textContent = `(${screen.textContent})`;
-
-          let tabl = {
-            operation: screen.textContent,
-            resultat: calcul,
-          };
-
-          let operationLocalStorage = JSON.parse(
-            localStorage.getItem("operation")
-          );
-
-          if (operationLocalStorage) {
-            operationLocalStorage.push(tabl);
-            localStorage.setItem(
-              "operation",
-              JSON.stringify(operationLocalStorage)
-            );
-          } else {
-            operationLocalStorage = [];
-            operationLocalStorage.push(tabl);
-            localStorage.setItem(
-              "operation",
-              JSON.stringify(operationLocalStorage)
-            );
-          }
-        }
+        equal();
         break;
       case "219":
         if (
@@ -198,42 +232,7 @@ const calculatrice = (valeur) => {
         }
     }
   } else if (valeur === "13") {
-    if (
-      lastC != "/" &&
-      lastC != "*" &&
-      lastC != "-" &&
-      lastC != "+" &&
-      lastC != "." &&
-      lastC != ""
-    ) {
-      gestionParenthese();
-
-      const calcul = eval(screen.textContent);
-      result.textContent = calcul.toLocaleString();
-      screen.textContent = `(${screen.textContent})`;
-
-      let tabl = {
-        operation: screen.textContent,
-        resultat: calcul,
-      };
-
-      let operationLocalStorage = JSON.parse(localStorage.getItem("operation"));
-
-      if (operationLocalStorage) {
-        operationLocalStorage.push(tabl);
-        localStorage.setItem(
-          "operation",
-          JSON.stringify(operationLocalStorage)
-        );
-      } else {
-        operationLocalStorage = [];
-        operationLocalStorage.push(tabl);
-        localStorage.setItem(
-          "operation",
-          JSON.stringify(operationLocalStorage)
-        );
-      }
-    }
+    equal();
   }
 };
 
@@ -242,10 +241,6 @@ window.addEventListener("error", (e) => {
   alert("Une erreur est survenue dans votre calcul : " + e.message);
 });
 
-// faire une fonction pour les different boutton egal. Optiumisation du code
-
-// Revoir css pour ameliorer la lisibilité de l'historique
-
-// Commenter mon historique.js
-
 // Implémenter une fonctionnalité de calcul de devise avec une API
+
+// Implémenter un boutton qui permettera de changer entre dark et light mode. Actuellement se mode est automatique en fonction de l'heure.
